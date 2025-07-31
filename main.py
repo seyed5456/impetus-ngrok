1eac0a19d27ce39ddeacdc703b7022cea8aa46c5
seyed5456 <seyed1061ho@gmail.com>
2025-07-31 15:27:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-# نقطه شروع برنامه
-print("سلام Impetus!")
+from app import app
 
-# اینجا می‌تونی از ماژول‌های لازم import کنی
-# مثلا: import requests
-
-# تابع‌های اولیه پروژه رو بساز
-def greeting(name):
-    return f"سلام {name}!"
-
-# اجرای تست اولیه
-if __name__ == "__main__":
-    print(greeting("Seyed"))
+if __name__ == '__main__':
+    app.run(debug=True)
