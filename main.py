83b252d0b91648a6a7c883ada6fc35052319fe9b
seyed5456 <seyed1061ho@gmail.com>
2025-07-31 15:05:13 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# نقطه شروع برنامه
+print("سلام Impetus!")
+
+# اینجا می‌تونی از ماژول‌های لازم import کنی
+# مثلا: import requests
+
+# تابع‌های اولیه پروژه رو بساز
+def greeting(name):
+    return f"سلام {name}!"
+
+# اجرای تست اولیه
+if __name__ == "__main__":
+    print(greeting("Seyed"))
